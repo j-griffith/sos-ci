a1dda4b86f59122475955c6fd04dfbeb635cc5b1
John Griffith <john.griffith@solidfire.com>
2014-09-06 08:37:30 -0600

    Clean up upload logs
    Make upload_logs work reliably with a uniqe name
    (based off of instance_name).
    
    Also stub in the results reporting/voting for gerrit
    in os_ci.py
    
    Add a "testit.py" to test running the executor by
    itself without a stream.

--- sos-ci/executor.py
+++ sos-ci/executor.py
@@ -1,10 +1,15 @@
 import subprocess
 
-# We'll move the actual work of deploying devstack and running tempest here
-# Same idea, make it a class that can be over-ridden with your own version of
-# tool chain.
+""" EZ-PZ just call our ansible playbook.
 
+Would be great to write a playbook runner at some point, but
+this is super straight forward and it works so we'll use it for now.
 
+TODO:
+* Clean up the vars mess we've got going on
+  Make things more dynamic and configurable (env vars?)
+
+"""
 
 def just_doit(patchset_ref):
     """ Do the dirty work. """
@@ -17,16 +22,7 @@ def just_doit(patchset_ref):
     vars += " instance_name=%s image_id=1f91f80e-9d0b-4466-b79c-65ef4b09a869" % ref_name
     vars += " patchset_ref=%s" % patchset_ref
     cmd = '/usr/local/bin/ansible-playbook --extra-vars \"%s\" ./ansible/sites.yml' % vars
-    #cmd = 'ls'
 
     p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
     output = p.stdout.read()
-    print output
-    #process = subprocess.Popen(cmd,
-    #                           stdout=subprocess.PIPE,
-    #                           stderr=subprocess.PIPE)
-    #results = process.communicate()[0]
-    #import pdb;pdb.set_trace()
-    #output = process.stdout.read()
-    #print output
-
+    return output
