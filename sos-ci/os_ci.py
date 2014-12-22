d6748b9e7466d0d04e6931e50a7aa33b32a710aa
John Griffith <john.griffith8@gmail.com>
2014-12-22 15:48:41 -0700

    Update repo with what's been running the last few months

--- sos-ci/os_ci.py
+++ sos-ci/os_ci.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 from collections import deque
-import datetime
 import json
 from optparse import OptionParser
 import os
@@ -31,16 +30,12 @@ class InstanceBuildException(Exception):
         Exception.__init__(self, message)
 
 def _filter_cinder_events(event):
-
-    with open('/home/jgriffith/sos_ci_events.dat', 'a') as f:
-        f.write('%s\n' % event)
-
     if (event.get('type', 'nill') == 'comment-added' and
         'Verified+1' in event['comment'] and
         PROJECT in event['change']['project']):
         if event['author']['username'] == 'jenkins':
-            print ('%s: Adding review id %s to job queue...' %
-                   (datetime.datetime.now(), event['change']['number']))
+            print ('Adding review id %s to job queue...' %
+                   event['change']['number'])
             return event
     else:
         return None
@@ -50,12 +45,10 @@ class JobThread(Thread):
     """ Thread to process the gerrit events. """
 
     def _post_results_to_gerrit(self, log_location, passed, commit_id):
-        #ssh -p 29418 USERNAME@review.openstack.org gerrit review -m '"Test failed on MegaTestSystem <http://megatestsystem.org/tests/1234>"' --verified=-1 c0ff33
         cmd = "gerrit review -m "
         failed = False
         if commit_id:
             if passed:
-                #cmd = """'"Test sfci-dsvm-volume passed on SolidFire CI System %s"' --verified +1 %s""" % (log_location, commit_id)
                 cmd += """"* solidfire-dsvm-volume %s : SUCCESS " %s""" % (log_location, commit_id)
             else:
                 cmd += """"* solidfire-dsvm-volume %s : FAILED " %s""" % (log_location, commit_id)
@@ -66,7 +59,6 @@ class JobThread(Thread):
 
         with open('/home/jgriffith/sos_ci_results.dat', 'a') as f:
             f.write('%s\n' % cmd)
-        #return
         if failed:
             return
 
@@ -100,7 +92,7 @@ class JobThread(Thread):
         while True:
             if not event_queue:
                 time.sleep(60)
-                print "%s: Waiting for event..." % datetime.datetime.now()
+                print "loop...debug"
             else:
                 event = event_queue.popleft()
 
@@ -120,27 +112,14 @@ class JobThread(Thread):
                 except InstanceBuildException:
                     pass
 
-                print "%s: Completed %s-dsvm-full" % (CI_NAME, datetime.datetime.now())
+                print "Completed %s-dsvm-full" % CI_NAME
                 url_name = patchset_ref.replace('/', '-')
                 log_location = 'http://54.164.167.86/solidfire-ci-logs/%s' % url_name
                 self._post_results_to_gerrit(log_location, success, commit_id)
-                #print "Events in queue: %s" % len(event_queue)
-                #self._post_results_to_gerrit('http://54.164.167.86/solidfire-ci-logs/%s' % 'foo',
-                #                             success,
-                #                             commit_id)
                 try:
                     pipeline.remove(valid_event)
                 except ValueError:
                     pass
-
-                # So if there's nothing in event_queue and nothing in progress
-                # should be a great time to delete and purge everything on the
-                # backend device
-
-                #if len(event_queue) == 0 and len(pipeline) == 0:
-                cmd = '/usr/local/bin/ansible-playbook ./ansible/cleanup_test_cluster.yml'
-                ansible_proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-                ansible_proc.communicate()[0]
 
 
 class GerritEventStream(object):
@@ -183,7 +162,7 @@ def process_options():
 
     parser.add_option('-n', '--num-threads', action='store',
                       type='int',
-                      default=1,
+                      default=2,
                       dest='number_of_worker_threads',
                       help='Number of job threads to run (default = 2).')
     parser.add_option('-m', action='store_true',
@@ -206,5 +185,5 @@ if __name__ == '__main__':
             valid_event = _filter_cinder_events(event)
             if valid_event:
                 if not options.event_monitor_only:
-                    print "%s: Adding event to queue..." % datetime.datetime.now()
+                    print "Adding event to queue..."
                     event_queue.append(valid_event)
